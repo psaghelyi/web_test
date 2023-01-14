7f8be05cb38f2d37d093738e22c9bd31486fa40d
psaghelyi <psaghelyi@gmail.com>
2023-01-14 20:31:32 +0100

    fixed no-cach http header response

--- server/web_rubyrails/application_controller.rb
+++ server/web_rubyrails/application_controller.rb
@@ -1,6 +1,5 @@
 require 'net/http'
 require 'benchmark'
-require 'prometheus/client'
 
 
 class ApplicationController < ActionController::API
@@ -41,13 +40,13 @@ class ApplicationController < ActionController::API
 
     def no_cache
         response.headers["Last-Modified"] = Time.now.httpdate
-        response.headers["Expires"] = 0
+        response.headers["Expires"] = "0"
 
         # HTTP 1.0
         response.headers["Pragma"] = "no-cache"
 
         # HTTP 1.1 'pre-check=0, post-check=0' (IE specific)
-        response.headers["Cache-Control"] = 'no-store, no-cache, must-revalidate,
-        max-age=0, pre-check=0, post-check=0'
+        response.headers["Cache-Control"] = 
+            "no-store, no-cache, must-revalidate, max-age=0, pre-check=0, post-check=0"
     end
 end
